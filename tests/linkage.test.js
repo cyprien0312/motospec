5cfb78e91beefb57b4dfc55d3fc0bf71224d4a4e
cyprien0312 <fwuak15@outlook.com>
2026-05-06 11:19:23 +1000

    Revert linkage test import drop (imports were used)

--- tests/linkage.test.js
+++ tests/linkage.test.js
@@ -1,6 +1,12 @@
 import { test } from 'node:test';
 import assert from 'node:assert/strict';
-import { closeFourBar } from '../src/linkage.js';
+import {
+  closeFourBar,
+  shockLength,
+  swingarmDeltaForShockTravel,
+  rearVerticalTravel,
+  rearRideHeight,
+} from '../src/linkage.js';
 
 test('4-bar closure: residual is zero at static configuration', () => {
   const cfg = {
